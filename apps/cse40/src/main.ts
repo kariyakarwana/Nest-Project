2d3b5cc01b9e74aab010dbf36afd4daf95776fae
kariyakarwana <kariyakarawanaz@gmail.com>
2025-05-03 10:18:45 +0530

    commit 4

--- apps/cse40/src/main.ts
+++ apps/cse40/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .addTag('basic-processing', 'Basic image processing operations')
     .addTag('enhancement', 'Image enhancement operations')
     .addTag('feature-detection', 'Image feature detection operations')
-    .build();
+    .build('api', app, document);
     
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
